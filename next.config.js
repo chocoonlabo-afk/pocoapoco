b54d4fc967066fcaead1a528bbea9f769a704641
unknown <chocoonlabo@gmail.com>
2025-11-22 21:39:04 +0900

    fix: turn off static export for pages using useSearchParams

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  // ここに output: 'export' は絶対に入れない
+  // output: 'standalone' も今回は不要です
 };
 
 module.exports = nextConfig;
